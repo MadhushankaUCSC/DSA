292c328415693b100055b4292cbdfa5345314f8c
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-11-07 09:18:45 +0530

    modify kmp algorithm file

--- String Matching Algorithms/2.kmp.c
+++ String Matching Algorithms/2.kmp.c
@@ -7,7 +7,28 @@ char *text;      //charactor pointer
 int s;
 int lps[6];   //integer arra for store longest prefix and suffix of the pattern
 int count;
+ //compute longest prefix and suffix
+int compLps(char str1[],int n,int lps[]){
 
+	int len=0;
+	int i=1;
+	lps[0]=0;
+	while(i<n){
+		if(str1[i] == str1[len]){
+			lps[i]=len+1;
+			len++;
+			i++;
+		}else{
+			if(len!=0){
+			len=lps[len-1];	
+			}else{
+				lps[i]=0;
+				i++;
+			}
+		}
+	}
+
+}
 
 //main function
 int main(){
