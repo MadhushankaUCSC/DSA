b813786db93d9ba50970321d52a76ed1f1f29f31
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-12-02 08:13:39 +0530

    add new file

--- String Matching Algorithms/3.Rabincrap.c
+++ String Matching Algorithms/3.Rabincrap.c
@@ -16,24 +16,6 @@ void rabinkarp(int n,int m){
   int txtHash=0; //hash value of text
   int h=1;
   int prime=101;  //prime number
-  #include <stdio.h>
-#include <string.h>
-#include<stdlib.h>
-
-#define k 256  //k is number of charactors in alphabet
-int i,j,m,n;
-char str1[]="960410";  //pattern
-char *text;
-int count;
-
-void rabinkarp(int n,int m){
-	
-	FILE *fg=fopen("results.txt","a");
-	
-  int patHash=0; //hash value of nPattern
-  int txtHash=0; //hash value of text
-  int h=1;
-  int prime=101;  //prime number
   
   
   //h value  pow(k,n-1)%q
@@ -123,37 +105,4 @@ int  main(int argc, char const *argv[]) {
   
    fclose(fp);
   return 0;
-}
-  
-  //h value  pow(k,n-1)%q
-  for(i=0;i <n-1;i++){
-    h=(h*k) % prime;
-  }
-  //compute hash value of text and pattern
-  for(i=0;i<n;i++){
-   patHash=(k*patHash + str1[i]) % prime;
-    txtHash=(k*txtHash + text[i]) % prime;
-  }
-
-
-  printf("\n\nTotal Number of matches:%d\n",count);  //print count of total matches
-   fprintf(fg,"\n\n\tTotal Number of matches:%d\n\n",count);  //print count of total matches to the results.txt file
-}
-
-
-
-  
-  fclose(fpoint);  //close file pointer
- // getch();
-  
-  m=strlen(text);  //get text length
-  n=strlen(str1);  //get pattern length
-  
-
-   
-
-   
-  rabinkarp(n,m);  //call rabinKarp function
-  
- 
 }
