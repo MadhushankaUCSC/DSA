5e54a0add50bcd6ad77c98839b0208f317203e7a
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-12-06 10:30:58 +0530

    modify

--- String Matching Algorithms/3.Rabincrap.c
+++ String Matching Algorithms/3.Rabincrap.c
@@ -5,8 +5,56 @@
 #define k 256  //k is number of charactors in alphabet
 int i,j,m,n;
 char str1[]="960410";  //pattern
-char *text;
-int count;
+
+
+
+void rabinkarp(int n,int m){
+	
+	FILE *fg=fopen("results.txt","a");
+	
+  int patHash=0; //hash value of nPattern
+  int txtHash=0; //hash value of text
+  int h=1;
+  int prime=101;  //prime number
+  
+  
+  //h value  pow(k,n-1)%q
+  for(i=0;i <n-1;i++){
+    h=(h*k) % prime;
+  }
+  //compute hash value of text and pattern
+  for(i=0;i<n;i++){
+   patHash=(k*patHash + str1[i]) % prime;
+    txtHash=(k*txtHash + text[i]) % prime;
+  }
+
+  for(i=0;i<=m-n;i++){
+    if(patHash == txtHash){
+      for(j=0;j<n;j++){
+        if(text[i+j] != str1[j])
+        break;
+      }
+
+      if(j==n){
+      	 printf("%d ,",i);  //print index of text match with the pattern
+      fprintf(fg,"%d, ",i);
+      count++;
+	  }
+      
+     
+    }
+
+    if(i<m-n){
+      txtHash= (k * (txtHash - text[i]*h) + text[i+n]) % prime;
+
+
+      if(txtHash<0)
+      txtHash=(txtHash+prime);
+    }
+  }
+  printf("\n\nTotal Number of matches:%d\n",count);  //print count of total matches
+   fprintf(fg,"\n\n\tTotal Number of matches:%d\n\n",count);  //print count of total matches to the results.txt file
+}
 
 
 int  main(int argc, char const *argv[]) {
@@ -37,8 +85,7 @@ int  main(int argc, char const *argv[]) {
   fclose(fpoint);  //close file pointer
  // getch();
   
-  m=strlen(text);  //get text length
-  n=strlen(str1);  //get pattern length
+ 
   
    FILE *fp=fopen("results.txt","a");
    
